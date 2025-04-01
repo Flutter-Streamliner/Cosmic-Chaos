655d7509c2a12f3891e9c1c752e97aca9400aff2
yehorme <yehorme@gmail.com>
2025-04-01 19:06:25 +0300

    feat: implement asteroid bounds and moving from edge to edge

--- lib/components/asteroid.dart
+++ lib/components/asteroid.dart
@@ -1,15 +1,21 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:cosmic_chaos/app_game.dart';
 import 'package:flame/components.dart';
 
 class Asteroid extends SpriteComponent with HasGameReference<AppGame> {
 
-  Asteroid({required super.position})
-    : super(size: Vector2.all(120), anchor: Anchor.center);
+  Asteroid({required super.position, double size = _maxSize})
+    : super(size: Vector2.all(size), anchor: Anchor.center, priority: -1) {
+    _velocity = _generateVelocity();
+  }
+
+  static const double _maxSize = 120;
 
   final Random _random = Random();
+  late Vector2 _velocity;
 
   @override
   FutureOr<void> onLoad() async {
@@ -20,12 +26,31 @@ class Asteroid extends SpriteComponent with HasGameReference<AppGame> {
 
   @override
   void update(double dt) {
-    position.y += 150 * dt;
+    position += _velocity * dt;
+
+    _handleScreenBounds();
+    super.update(dt);
+  }
+
+  Vector2 _generateVelocity() {
+    final double forceFactor = _maxSize / size.x;
+    return Vector2(
+      _random.nextDouble() * 120 - 60,
+      100 + _random.nextDouble() * 50,
+    ) * forceFactor;
+  }
+
+  void _handleScreenBounds() {
 
     // remove the asteroid from the game if it goes below the bottom
     if (position.y > game.size.y + size.y / 2) {
       removeFromParent();
     }
-    super.update(dt);
+    final double screenWidth = game.size.x;
+    if (position.x < -size.x / 2) {
+      position.x = screenWidth + size.x / 2;
+    } else if (position.x > screenWidth + size.x / 2) {
+      position.x = -size.x / 2;
+    }
   }
 }
